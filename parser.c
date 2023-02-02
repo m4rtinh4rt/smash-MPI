de40e3ba1c6c6cdbe3a0354418251c840c105238
vpatuta <114212613+vpatuta@users.noreply.github.com>
2023-02-02 12:09:00 +0100

    refactor: merge 2 parse config in one

--- parser.c
+++ parser.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -56,24 +57,56 @@ void *
 smash_parse_cfg(enum CFG ctype)
 {
 	void *data;
-	size_t data_size;
-	char *config_path;
+	int ret;
+	size_t data_size, nline, n_cfg;
+	char *config_path, *line, err_buf[100];
+	const char *rs;
+	regex_t r;
+	regmatch_t rm[5];
+	void (*f)(/*struct pointer, regmatch_t*/);
 
 	if (!(config_path = smash_get_config_path(ctype)))
-		goto err;
+		return NULL;
 
 	if (!(data = smash_load_file_in_memory(config_path, &data_size)))
-		goto err;
-
-	switch (ctype) {
-	case CFG_DELAY:
-		return smash_parse_delay_cfg(data, data_size);
-	case CFG_FAILURE:
-		return smash_parse_failure_cfg(data, data_size);
-	default:
-		/* never reached */
 		return NULL;
+
+	if (ctype == CFG_DELAY) {
+		rs = "([0-9]+);([0-9]+);([0-9]+);(-?[0-9]+)";
+		n_cfg = 5;
+		f = smash_populate_delay;
+	} else {
+		rs = "([0-9]+);([0-9]+)";
+		n_cfg = 3;
+		f = smash_populate_failure;
 	}
+
+	if ((ret = regcomp(&r, rs, REG_EXTENDED)) != 0) {
+		regerror(ret, &r, err_buf, 100);
+		fprintf(stderr, "failed to compile regex <%s>:%s\n", rs,
+			err_buf);
+		goto err;
+	}
+
+	nline = 1;
+	line = strtok(data, "\n");
+	while (line) {
+		// if line is a comment or snaphot, do smth
+		if ((ret = regexec(&r, line, n_cfg, rm, 0)) != 0) {
+			regerror(ret, &r, err_buf, 100);
+			fprintf(stderr, "line %ld: %s\n", nline, err_buf);
+			goto err;
+		}
+		// TODO: call f(&struct to populate)
+		nline++;
+		line = strtok(NULL, "\n");
+	}
+
+	regfree(&r);
+	munmap(data, data_size);
+	// TODO: return structure
 err:
+	regfree(&r);
+	munmap(data, data_size);
 	return NULL;
 }
