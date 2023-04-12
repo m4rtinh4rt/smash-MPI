7782b345e69aa0702db27b1ca2f29f52e58aeb36
Martin Hart <114212671+m4rtinh4rt@users.noreply.github.com>
2023-04-12 13:58:10 +0200

    feat: add __libc_start_main hooking and hijacking logic for MPI_Send

--- hooking.c
+++ hooking.c
@@ -1,7 +1,15 @@
 #include <dlfcn.h>
 #include <err.h>
 #include <gnu/lib-names.h>
+#include <mpi.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "callout.h"
+#include "hooking.h"
+#include "parser.h"
 
 void *
 smash_get_lib_func(const char *lname, const char *fname)
@@ -16,4 +24,74 @@ smash_get_lib_func(const char *lname, const char *fname)
 
 	dlclose(lib);
 	return p;
+}
+
+static void
+smash_handler(int signum)
+{
+	puts("SIGALRM");
+	smash_clock();
+	alarm(1);
+}
+
+void
+smash_setup_alarm(void)
+{
+	struct sigaction sa;
+
+	sa.sa_handler = smash_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+
+	sigaction(SIGALRM, &sa, NULL);
+	alarm(1);
+}
+
+int
+__libc_start_main(
+    int (*main)(int, char **, char **),
+    int argc,
+    char **argv,
+    int (*init)(int, char **, char **),
+    void (*fini)(void),
+    void (*rtld_fini)(void),
+    void *stack_end)
+{
+    int (*f)();
+
+    f = smash_get_lib_func(LIBM_SO, "__libc_start_main");
+    smash_setup_alarm();
+    return f(main, argc, argv, init, fini, rtld_fini, stack_end);
+}
+
+int
+MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
+             int tag, MPI_Comm comm)
+{
+	int (*f)(), my_rank;
+	unsigned int i;
+	struct cfg_delays *delays;
+	struct mpi_send_args args = {
+		.buf = (void *)buf,
+		.count = count,
+		.datatype = datatype,
+		.dest = dest,
+		.tag = tag,
+		.comm = comm,
+	};
+
+	f = smash_get_lib_func(LIBMPI, "MPI_Send");
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	delays = smash_parse_cfg(CFG_DELAY);
+	if (delays == NULL) {
+		fprintf(stderr, "error in CFG_DELAY\n");
+		exit(1);
+	}
+	for (i = 0; i < delays->size; ++i) {
+		if (delays->delays[i].dst == (unsigned int)dest && delays->delays[i].src == (unsigned int)my_rank) {
+			smash_timeout(f, 6, delays->delays[i].delay, &args);
+			return 0;
+		}
+	}
+	return f(buf, count, datatype, dest, tag, comm);
 }
